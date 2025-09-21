b388c41f0c2ee161e3f72ad1e284c32c47a18265
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-09-21 15:27:29 -0500

    Hecho por SoyMaycol

---
+++ Adonix-API-89d6480633803b44e88caa8efd546ef820872c8c/api/pinvid/index.js
@@ -0,0 +1,61 @@
+import express from 'express'
+import fetch from 'node-fetch'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/api/pinvid', async (req, res) => {
+  const query = req.query.q
+  const show = req.query.show || 5
+  const apikey = 'GataDios'
+  const url = `https://api.neoxr.eu/api/pinterest-v2?q=${encodeURIComponent(query)}&show=${show}&type=video&apikey=${apikey}`
+
+  if (!query) {
+    return res.status(400).json({
+      status: 400,
+      error: '❌ Faltó el parámetro ?q= para buscar'
+    })
+  }
+
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    if (!data.status || !data.data) {
+      return res.status(500).json({
+        status: 500,
+        error: '❌ Error al obtener resultados',
+        detalle: data?.msg || 'Sin detalles'
+      })
+    }
+
+    const videos = data.data.map((v, i) => ({
+      titulo: v.title || 'Sin título',
+      creador: 'Ado ( Wirk )',
+      autor: v.author.full_name || 'Desconocido',
+      usuario: '@' + v.author.username,
+      thumbnail: v.content[0]?.thumbnail || null,
+      video: v.content[0]?.url || null,
+      fuente: v.source
+    }))
+
+    return res.status(200).json({
+      status: 200,
+      resultados: videos.length,
+      query,
+      videos
+    })
+
+  } catch (err) {
+    console.error('❌ Error al buscar en Pinterest:', err)
+    return res.status(500).json({
+      status: 500,
+      error: '⚠️ Error al hacer la búsqueda',
+      detalle: err.message
+    })
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor listo en http://localhost:${PORT}`)
+})
